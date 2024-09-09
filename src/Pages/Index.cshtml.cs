a517b04b34c27042bae2c59c14b2c923054a6da0
Jose A. Fernandez <fernandezja@gmail.com>
2024-09-09 20:07:59 -0300

    App: Add OS description (Operating system version, platform, servicepack)

--- src/Pages/Index.cshtml.cs
+++ src/Pages/Index.cshtml.cs
@@ -7,6 +7,9 @@ public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
 
+    public string OperatingSystemNameAndVersion { get; set; }
+    public OperatingSystem OperatingSystem { get; set; }
+
     public IndexModel(ILogger<IndexModel> logger)
     {
         _logger = logger;
@@ -14,6 +17,9 @@ public class IndexModel : PageModel
 
     public void OnGet()
     {
+        OperatingSystemNameAndVersion = System.Runtime.InteropServices.RuntimeInformation.OSDescription;
+
+        OperatingSystem = new OperatingSystem(System.Environment.OSVersion.Platform, new Version());
 
     }
 }
